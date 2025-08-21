db9b4886836d286a87a1b538bb0f6e915f8c9d83
Laboratório FIAP <labsfiap@fiap.com.br>
2025-08-20 22:37:35 -0300

    feat: estruturacao do projeto e readme

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
+import { ExerciseList } from "./components/ExerciseList"
 import { MotivationalMessage } from "./components/motivationmessage"
+import { Welcome } from "./components/welcome"
 
 function App() {
 
   return (
     <>
-      <main>
+        <Welcome />
+        <ExerciseList />
         <MotivationalMessage message='"A vida é como um treino: quanto mais você se dedica melhores são os resultados"' author="Chris Bumstead"/>
-      </main>
     </>
   )
 }
