56fd47edce6792acc4dc16fd38849ee7850aa363
gkenji110 <gustavo.terada01@gmail.com>
2025-08-20 22:20:00 -0300

    feat: created motivation message component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
+import { MotivationalMessage } from "./components/motivationmessage"
+
 function App() {
 
   return (
     <>
-     
+      <main>
+        <MotivationalMessage message='"A vida é como um treino: quanto mais você se dedica melhores são os resultados"' author="Chris Bumstead"/>
+      </main>
     </>
   )
 }
